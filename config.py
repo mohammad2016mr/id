8803ff860e12f93fbe2e436d69a8d7a72f2ab25b
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-08-02 05:40:41 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 
-token = 'Your Token' # Add Your Token
-is_sudo = 'ID Admin' # add Your ID
+token = '262945229:AAHuCAq8451ZJs7QKM71AkEDIMMza1ndj1E' # Add Your Token
+is_sudo = '90516804.172561830' # add Your ID
 
 
 
-# _____           _              _____
-#|_   _|_ _ _   _| | ___  _ __  |_   _|__  __ _ _ __ ___
-#  | |/ _` | | | | |/ _ \| '__|   | |/ _ \/ _` | '_ ` _ \
-#  | | (_| | |_| | | (_) | |      | |  __/ (_| | | | | | |
-#  |_|\__,_|\__, |_|\___/|_|      |_|\___|\__,_|_| |_| |_|
-#           |___/
-#Copy right  2016 Negative - Taylor Team
-#MIT license
